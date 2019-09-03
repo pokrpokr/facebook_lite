353d8762fc6b626b889126c7d7cc0e7cdc65a18f
chai <yeshengdemogujun@gmail.com>
2019-09-03 17:14:10 +1000

    change

--- views/login.php
+++ views/login.php
@@ -1,7 +1,41 @@
+<!<!DOCTYPE html >
 <html>
+<head>
+    <script src="/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="/node_modules/bootstrap/dist/css/bootstrap.min.css"></script> 
+</head>
 <body>
+    <div class = 'container-fluid'>
+        <div class = "row">
+            <div class="col-md-1 col-md-offset-1">.col-md-1</div>
+                Facebook Lite
+            </div>
+        </div>
+        
+
+        <div>
+        
+        </div>
+
+        <div>
+        
+    </div>
+
+    <div class="navbar">
+  <div class="navbar-inner">
+    <a class="brand" href="#">Title</a>
+    <ul class="nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+    </ul>
+  </div>
+</div>
 <?php
 echo "please login ";
 ?>
 </body>
 </html>
+
+
